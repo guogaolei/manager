c6b6659aa153eb5121b6adc696902f6d88b4d356
guohaolei <462919246@qq.com>
2021-06-11 20:40:04 +0800

    增加jar启动指令

--- src/main/java/com/guohl/innermanage/InnermanageApplication.java
+++ src/main/java/com/guohl/innermanage/InnermanageApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InnermanageApplication {
 
+    //nohup java -jar xxx.jar > catalina.out 2>&1
     public static void main(String[] args) {
         SpringApplication.run(InnermanageApplication.class, args);
     }
