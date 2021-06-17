77fbd96863251c02bd0e02719253f6132e183c39
guohaolei <462919246@qq.com>
2021-06-17 13:47:59 +0800

    增加jar启动指令

--- src/main/java/com/guohl/innermanage/InnermanageApplication.java
+++ src/main/java/com/guohl/innermanage/InnermanageApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InnermanageApplication {
 
-    //nohup java -jar xxx.jar > catalina.out 2>&1
+    //nohup java -jar xxx.jar > catalina.out 2>&1 & 如不用此命令启动jar包，则服务运行一段时间就会停止
     public static void main(String[] args) {
         SpringApplication.run(InnermanageApplication.class, args);
     }
