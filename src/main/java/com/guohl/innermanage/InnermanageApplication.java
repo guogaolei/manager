e35cca6f640c1a96a58442690f5e0ef80cf747d4
guohaolei <462919246@qq.com>
2021-06-21 15:35:58 +0800

    增加扫描dao包

--- src/main/java/com/guohl/innermanage/InnermanageApplication.java
+++ src/main/java/com/guohl/innermanage/InnermanageApplication.java
@@ -1,9 +1,11 @@
 package com.guohl.innermanage;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.guohl.innermanage.dao")
 public class InnermanageApplication {
 
     //nohup java -jar xxx.jar > catalina.out 2>&1 &  如不用此命令启动jar包，则服务运行一段时间就会停止
